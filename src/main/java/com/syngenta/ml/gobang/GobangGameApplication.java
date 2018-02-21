4e7a4e89d203eb8a572b4ec6a3440cc6a0bea3ba
pengmu <mupeng4@hotmail.com>
2018-02-21 16:53:00 +0100

    Test a change
    a test change

--- src/main/java/com/syngenta/ml/gobang/GobangGameApplication.java
+++ src/main/java/com/syngenta/ml/gobang/GobangGameApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GobangGameApplication {
 
 	public static void main(String[] args) {
+		//this is a change
 		SpringApplication.run(GobangGameApplication.class, args);
 	}
 }
